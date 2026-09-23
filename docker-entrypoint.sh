e357b730a2f8d8e6c3dc499e0c9cebe64adc3267
besoeasy <besoeasy@users.noreply.github.com>
2026-09-24 03:38:04 +0530

    build: add IPFS and web app entrypoint

---
+++ docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -u
+
+ipfs daemon --init --init-profile=lowpower &
+ipfs_pid=$!
+
+stop_ipfs() {
+	if kill -0 "$ipfs_pid" 2>/dev/null; then
+		kill -TERM "$ipfs_pid" 2>/dev/null || true
+	fi
+}
+
+trap stop_ipfs TERM INT
+
+/usr/local/bin/originless &
+app_pid=$!
+
+wait "$app_pid"
+app_status=$?
+
+stop_ipfs
+wait "$ipfs_pid" 2>/dev/null || true
+exit "$app_status"
